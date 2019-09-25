a606e3255d8418b594b2dca1d7c234ea3b5db880
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2019-09-25 14:48:51 +0700

    Add files via upload

---
+++ IF1-10118023-Latihan8-Pecahan/src/latihan8/Latihan8.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan8;
+
+/**
+ * @author Zaenal PC
+ * NAMA              : Zaenal Anzarry
+ * KELAS             : IF-01
+ * NIM               : 10118023
+ * Deskripsi Program : Pecahan
+ */
+
+public class Latihan8 {
+
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
